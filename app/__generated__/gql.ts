b2e5f798392591c75444c333c82303c3fe53a021
Aditya Kumar <adikumar306@gmail.com>
2024-01-05 09:28:41 +0530

    cache write fragment on vote, render vote status

--- app/__generated__/gql.ts
+++ app/__generated__/gql.ts
@@ -14,7 +14,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  */
 const documents = {
     "fragment FieldErrors on FieldError {\n  field\n  message\n}": types.FieldErrorsFragmentDoc,
-    "fragment PostFields on Post {\n  id\n  title\n  author {\n    ...UserFields\n  }\n  points\n  createdAt\n  updatedAt\n  textSlice\n}": types.PostFieldsFragmentDoc,
+    "fragment PostFields on Post {\n  id\n  title\n  author {\n    ...UserFields\n  }\n  points\n  createdAt\n  updatedAt\n  textSlice\n  voteStatus\n}": types.PostFieldsFragmentDoc,
     "fragment UserFields on User {\n  id\n  username\n  email\n  fname\n  lname\n  createdAt\n  updatedAt\n}": types.UserFieldsFragmentDoc,
     "mutation ChangePassword($options: PasswordResetInput!) {\n  changePassword(options: $options) {\n    errors {\n      field\n      message\n    }\n    user {\n      id\n      username\n      email\n      fname\n      lname\n      createdAt\n      updatedAt\n    }\n  }\n}": types.ChangePasswordDocument,
     "mutation CreatePost($input: PostInput!) {\n  createPost(input: $input) {\n    id\n    title\n    text\n    author {\n      id\n      username\n      email\n      fname\n      lname\n      createdAt\n      updatedAt\n    }\n    points\n    createdAt\n    updatedAt\n  }\n}": types.CreatePostDocument,
@@ -22,7 +22,7 @@ const documents = {
     "mutation Login($options: LoginInput!) {\n  login(options: $options) {\n    errors {\n      field\n      message\n    }\n    user {\n      id\n      username\n      email\n      fname\n      lname\n      createdAt\n      updatedAt\n    }\n  }\n}": types.LoginDocument,
     "mutation Logout {\n  logout\n}": types.LogoutDocument,
     "mutation Register($options: RegisterInput!) {\n  register(options: $options) {\n    errors {\n      field\n      message\n    }\n    user {\n      id\n      username\n      email\n      fname\n      lname\n      createdAt\n      updatedAt\n    }\n  }\n}": types.RegisterDocument,
-    "mutation Vote($input: VoteInput!) {\n  vote(input: $input)\n}": types.VoteDocument,
+    "mutation Vote($input: VoteInput!) {\n  vote(input: $input) {\n    ...PostFields\n  }\n}": types.VoteDocument,
     "query GetAllPosts($options: PaginationInput!) {\n  getAllPosts(options: $options) {\n    posts {\n      ...PostFields\n    }\n    hasMore\n  }\n}": types.GetAllPostsDocument,
     "query getUserDetails($username: String!) {\n  getUserDetails(username: $username) {\n    errors {\n      ...FieldErrors\n    }\n    user {\n      ...UserFields\n    }\n  }\n}": types.GetUserDetailsDocument,
     "query isLoggedIn {\n  isLoggedIn {\n    errors {\n      ...FieldErrors\n    }\n    user {\n      ...UserFields\n    }\n  }\n}": types.IsLoggedInDocument,
@@ -49,7 +49,7 @@ export function gql(source: "fragment FieldErrors on FieldError {\n  field\n  me
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(source: "fragment PostFields on Post {\n  id\n  title\n  author {\n    ...UserFields\n  }\n  points\n  createdAt\n  updatedAt\n  textSlice\n}"): (typeof documents)["fragment PostFields on Post {\n  id\n  title\n  author {\n    ...UserFields\n  }\n  points\n  createdAt\n  updatedAt\n  textSlice\n}"];
+export function gql(source: "fragment PostFields on Post {\n  id\n  title\n  author {\n    ...UserFields\n  }\n  points\n  createdAt\n  updatedAt\n  textSlice\n  voteStatus\n}"): (typeof documents)["fragment PostFields on Post {\n  id\n  title\n  author {\n    ...UserFields\n  }\n  points\n  createdAt\n  updatedAt\n  textSlice\n  voteStatus\n}"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
@@ -81,7 +81,7 @@ export function gql(source: "mutation Register($options: RegisterInput!) {\n  re
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(source: "mutation Vote($input: VoteInput!) {\n  vote(input: $input)\n}"): (typeof documents)["mutation Vote($input: VoteInput!) {\n  vote(input: $input)\n}"];
+export function gql(source: "mutation Vote($input: VoteInput!) {\n  vote(input: $input) {\n    ...PostFields\n  }\n}"): (typeof documents)["mutation Vote($input: VoteInput!) {\n  vote(input: $input) {\n    ...PostFields\n  }\n}"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- app/home/vote.tsx
+++ app/home/vote.tsx
@@ -2,7 +2,7 @@
 
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons"
 import { Box, Flex, IconButton, Text } from "@chakra-ui/react"
-import { Post, PostFieldsFragment, VoteDocument } from "../__generated__/graphql"
+import { PostFieldsFragment, PostFieldsFragmentDoc, VoteDocument } from "../__generated__/graphql"
 import { useMutation } from "@apollo/client"
 import { useState } from "react";
 
@@ -14,10 +14,30 @@ interface VoteProps {
 export const Vote:React.FC<VoteProps> = ({post}) => {
     const [loading, setLoading] = 
         useState<'upvoteLoading' | 'downvoteLoading' | 'notLoading'>('notLoading');
-    const [vote] = useMutation(VoteDocument);
+
+    const [vote] = useMutation(VoteDocument, {
+        update(cache, {data}) {
+            cache.modify({
+                fields: {
+                    getAllPosts(existingPosts = [], { readField }) {
+                        const updatedPostRef = cache.writeFragment({
+                            data: data?.vote,
+                            fragment: PostFieldsFragmentDoc,
+                            fragmentName: "PostFields" 
+                            //give fragment name when more than 1 fragment is involved in the cache update
+                        })
+                        return existingPosts;
+                    }
+                }
+            })
+        }
+    });
 
     const upvote = async() => {
-        // vote mutation should run with postId - take it from props, value should be +1
+        // vote mutation should run with postId - take it from props, value should be +1    
+
+        // after doing a vote - vote mutation should return me the updated post with updated points
+        // this post's cache id will be retrieved and PostFieldsFragment will be written in update callback
 
         setLoading('upvoteLoading');
         await vote({
@@ -41,7 +61,7 @@ export const Vote:React.FC<VoteProps> = ({post}) => {
                     postId: post.id,
                     vote: -1
                 }
-            }
+            },
         });
         setLoading('notLoading');
     }
@@ -49,9 +69,19 @@ export const Vote:React.FC<VoteProps> = ({post}) => {
     return (
         // two icon buttons 
         <Flex direction={"column"} m={3} alignContent={"space-evenly"}>
-            <IconButton isLoading={loading == 'upvoteLoading' ? true : false}  aria-label="upvote" icon={<ChevronUpIcon />} onClick={upvote}></IconButton>
+            <IconButton 
+                isLoading={loading == 'upvoteLoading' ? true : false}  
+                aria-label="upvote" icon={<ChevronUpIcon />} 
+                colorScheme={post.voteStatus == 1 ? "green" : "gray"}
+                onClick={upvote}>
+            </IconButton>
             <Text textAlign="center">{post.points}</Text>
-            <IconButton isLoading={loading == 'downvoteLoading' ? true : false} aria-label="downvote" icon={<ChevronDownIcon />} onClick={downvote}></IconButton>
+            <IconButton 
+                isLoading={loading == 'downvoteLoading' ? true : false} 
+                aria-label="downvote" icon={<ChevronDownIcon />} 
+                colorScheme={post.voteStatus == -1 ? "red" : "gray"}
+                onClick={downvote}>
+            </IconButton>
         </Flex>
     )
 }
